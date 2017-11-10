17b6a05631f59535f1c752a60b4985ebfb6b14d0
Minijackson <minijackson@riseup.net>
2017-11-10 13:55:19 -0500

    Bump version to 0.1.2
    Enhancements:
    - Better Logging
    
    Fixes:
    - Do not crash on uncommon simple_bind errors (authenticate)

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Paddle.Mixfile do
 
   def project do
     [app: :paddle,
-     version: "0.1.1",
+     version: "0.1.2",
      description: "A library simplifying LDAP usage",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
